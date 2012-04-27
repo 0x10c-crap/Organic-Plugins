2847a13d4b9c4158d486a6e923b60092380200c4
Drew DeVault <sir@cmpwn.com>
2012-04-27 01:22:06 -0600

    Initial commit

---
+++ 0x10co.de/0x10co.de/0x10co.de/Plugin.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using orgASM.Plugins;
+using orgASM;
+using System.Net;
+using System.IO;
+using System.Windows.Forms;
+
+namespace _0x10co.de
+{
+    public class Plugin : IPlugin
+    {
+        public string Description
+        {
+            get { return "Uploads output to 0x10co.de"; }
+        }
+
+        private bool upload;
+
+        public void Loaded(Assembler assembler)
+        {
+            assembler.AssemblyComplete += new EventHandler<AssemblyCompleteEventArgs>(assembler_AssemblyComplete);
+            assembler.TryHandleParameter += new EventHandler<HandleParameterEventArgs>(assembler_TryHandleParameter);
+            assembler.AddHelpEntry("0x10co.de:\n" +
+                "\t--0x10co.de: Upload the output to 0x10co.de automatically.");
+        }
+
+        void assembler_TryHandleParameter(object sender, HandleParameterEventArgs e)
+        {
+            if (e.Parameter == "--0x10co.de")
+                e.Handled = upload = true;
+        }
+
+        void assembler_AssemblyComplete(object sender, AssemblyCompleteEventArgs e)
+        {
+            // uploads a .dat file
+            if (!upload || e.Output.Count == 0)
+                return;
+            Console.WriteLine("Uploading output to 0x10co.de...");
+            string code = "";
+            foreach (var entry in e.Output)
+            {
+                if (entry.Output == null)
+                    continue;
+                if (entry.Output.Length != 0)
+                {
+                    string dat = "dat ";
+                    foreach (ushort value in entry.Output)
+                    {
+                        dat += "0x" + value.ToString("x") + ",";
+                    }
+                    dat = dat.Remove(dat.Length - 1) + "\t; " + entry.Code;
+                    code += dat + "\n";
+                }
+            }
+            HttpWebRequest hwr = (HttpWebRequest)WebRequest.Create(new Uri("http://0x10co.de"));
+            hwr.ContentType = "application/x-www-form-urlencoded";
+            hwr.Method = "POST";
+            string postData = "title=" + Uri.EscapeDataString(e.Output.First().FileName) + "&author=&description=Created+by+the+0x10co.de+.orgASM+plugin&password=&code=" + Uri.EscapeDataString(code);
+            byte[] data = Encoding.ASCII.GetBytes(postData);
+            hwr.ContentLength = data.Length;
+            Stream s = hwr.GetRequestStream();
+            s.Write(data, 0, data.Length);
+            s.Close();
+
+            HttpWebResponse resp = (HttpWebResponse)hwr.GetResponse();
+            s = resp.GetResponseStream();
+            MemoryStream ms = new MemoryStream();
+            int b = 0;
+            while ((b = s.ReadByte()) != -1)
+                ms.WriteByte((byte)b);
+
+            string url = Encoding.ASCII.GetString(ms.GetBuffer()).Trim(' ', '\t', '\0');
+            Console.WriteLine("Uploaded to http://0x10co.de" + url + ".  This URL has been copied to the clipboard.");
+            Clipboard.SetText("http://0x10co.de" + url);
+        }
+
+        public string Name
+        {
+            get { return "0x10co.de"; }
+        }
+
+        public Version Version
+        {
+            get { return new Version(1, 0); }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+.orgASM-Plugins
+===============
+
+This repository contains the source code for several useful .orgASM plugins, and the repository is (will be) visible from the plugin installer.  Here's a list of plugins and what they do:
+
+* **0x10co.de**: This plugin allows you to automatically upload .orgASM output to 0x10co.de.
+
+Submitting Plugins
+------------------
+
+If you wish to add your plugin, submit a pull request and I will review and add it at our discresion.  You must target the .NET 3.5 Framework.
